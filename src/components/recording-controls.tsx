d6b7065dfc82ce9a823c2c00822051f340797601
ifindev <arifin.muhammad2610@Gmail.com>
2025-04-30 20:32:50 +0700

    feat: update RecordingControls component layout and enhance button functionality with animation

--- src/components/recording-controls.tsx
+++ src/components/recording-controls.tsx
@@ -21,28 +21,32 @@ export default function RecordingControls({
     isRecording,
 }: Props) {
     return (
-        <div className="flex items-center gap-4 bg-gray-100 rounded-full p-2 px-4">
-            <div className="text-sm">{formatTimeToMinutesAndSeconds(recordingTime)}</div>
-            <div className="flex-1 text-center text-sm">{isPaused ? 'Paused' : 'Recording'}</div>
-            <div className="flex gap-2">
-                {handlePauseRecording && handleResumeRecording && (
+        <div className="w-full flex items-center justify-center">
+            <div className="flex items-center gap-4 bg-gray-100 rounded-full p-2 px-4 animate-expand-center">
+                <div className="text-sm">{formatTimeToMinutesAndSeconds(recordingTime)}</div>
+                <div className="flex-1 text-center text-sm">
+                    {isPaused ? 'Paused' : 'Recording'}
+                </div>
+                <div className="flex gap-2">
+                    {handlePauseRecording && handleResumeRecording && (
+                        <Button
+                            variant="ghost"
+                            size="icon"
+                            className="rounded-full size-9"
+                            onClick={isPaused ? handleResumeRecording : handlePauseRecording}
+                        >
+                            {isPaused ? <Play className="size-4" /> : <Pause className="size-4" />}
+                        </Button>
+                    )}
                     <Button
                         variant="ghost"
                         size="icon"
                         className="rounded-full size-9"
-                        onClick={isPaused ? handleResumeRecording : handlePauseRecording}
+                        onClick={handleStopRecording}
                     >
-                        {isPaused ? <Play className="size-4" /> : <Pause className="size-4" />}
+                        <StopCircle className="size-4" />
                     </Button>
-                )}
-                <Button
-                    variant="ghost"
-                    size="icon"
-                    className="rounded-full size-9"
-                    onClick={handleStopRecording}
-                >
-                    <StopCircle className="size-4" />
-                </Button>
+                </div>
             </div>
         </div>
     );
